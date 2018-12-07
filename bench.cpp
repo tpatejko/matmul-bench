b1da8d537d30f7e178205f90d1f3c2ede4e635ec
Tomasz Patejko <tomasz.patejko@intel.com>
2018-12-07 12:01:31 +0100

    Remove single execution and just stick to batched conf.

--- bench.cpp
+++ bench.cpp
@@ -57,132 +57,6 @@ duration_t<std::micro> calculate_average(const std::vector<duration_t<std::micro
   return total / measures.size();
 }
 
-class single {
-  void mkldnn_matmul(const mat_descriptor& dims_a, const float* a,
-		     const mat_descriptor& dims_b, const float* b,
-		     float* c,
-		     float alpha, float beta) {
-    char a_transposed = dims_a.transposed ? 'T' : 'N';
-    char b_transposed = dims_b.transposed ? 'T' : 'N';
-
-    auto m = dims_a.height;
-    auto n = dims_b.width;
-    auto k = dims_a.width;
-
-    auto lda = dims_a.transposed ? m : k;
-    auto ldb = dims_b.transposed ? k : n;
-    auto ldc = n;
-  
-    mkldnn_sgemm(&b_transposed, &a_transposed,
-		 &n, &m, &k,
-		 &alpha,
-		 b, &ldb,
-		 a, &lda,
-		 &beta,
-		 c, &ldc);
-  }
-  
-  void mkl_matmul(const mat_descriptor& dims_a, const float* a,
-		  const mat_descriptor& dims_b, const float* b,
-		  float* c,
-		  float alpha, float beta)
-  {
-    CBLAS_TRANSPOSE a_transposed = dims_a.transposed ? CblasTrans : CblasNoTrans;
-    CBLAS_TRANSPOSE b_transposed = dims_b.transposed ? CblasTrans : CblasNoTrans;
-
-    auto m = dims_a.height;
-    auto n = dims_b.width;
-    auto k = dims_a.width;
-  
-    auto lda = dims_a.transposed ? m : k;
-    auto ldb = dims_b.transposed ? k : n;
-    auto ldc = n;
-  
-    cblas_sgemm(CblasRowMajor, a_transposed, b_transposed,
-		m, n, k,
-		alpha,
-		a, lda,
-		b, ldb, beta,
-		c, ldc);
-  }
-
- public:
-  void operator()() {
-    constexpr int m = 2048;
-    constexpr int k = 2048;
-    constexpr int n = 64;
-    constexpr int warm_up = 10;
-    constexpr int iters = 10000;
-  
-    auto dims_a = create_matrix_descriptor({m, k}, false);
-    auto dims_b = create_matrix_descriptor({k, n}, false);
-
-    std::unique_ptr<float[]> ptr_a{new float[dims_a.height*dims_a.width]};
-    std::unique_ptr<float[]> ptr_b{new float[dims_b.height*dims_b.width]};
-
-    std::random_device rd;
-    std::mt19937 e{rd()};
-    std::uniform_real_distribution<float> dist{0, 1};
-
-    for (size_t i = 0; i < dims_a.height*dims_a.width; i++) {
-      ptr_a[i] = dist(e);
-    }
-
-    for (size_t i = 0; i < dims_b.height*dims_b.width; i++) {
-      ptr_b[i] = dist(e);
-    }
-
-    std::unique_ptr<float[]> mkldnn_ptr_c{new float[dims_a.height*dims_b.width]};
-    std::unique_ptr<float[]> mkl_ptr_c{new float[dims_a.height*dims_b.width]};
-
-    for (int i = 0; i < warm_up; i++) {
-      mkldnn_matmul(dims_a, ptr_a.get(),
-		    dims_b, ptr_b.get(),
-		    mkldnn_ptr_c.get(),
-		    1.0, 0.0);
-
-      mkl_matmul(dims_a, ptr_a.get(),
-		 dims_b, ptr_b.get(),
-		 mkl_ptr_c.get(),
-		 1.0, 0.0);
-    }
-    
-    std::vector<std::chrono::duration<double, std::micro>> mkldnn_durs;
-    std::vector<std::chrono::duration<double, std::micro>> mkl_durs;
-    
-    for (int i = 0; i < iters; i++) {
-      auto s = std::chrono::high_resolution_clock::now();
-      mkldnn_matmul(dims_a, ptr_a.get(),
-		    dims_b, ptr_b.get(),
-		    mkldnn_ptr_c.get(),
-		    1.0, 0.0);
-      auto e = std::chrono::high_resolution_clock::now();
-      mkldnn_durs.push_back(e-s);
-    }
-
-    for (int i = 0; i < iters; i++) {
-      auto s = std::chrono::high_resolution_clock::now();
-      mkl_matmul(dims_a, ptr_a.get(),
-		 dims_b, ptr_b.get(),
-		 mkl_ptr_c.get(),
-		 1.0, 0.0);
-      auto e = std::chrono::high_resolution_clock::now();
-      mkl_durs.push_back(e-s);
-    }
-    
-    std::cout << "Single execution\n";
-    std::cout << "Avg. per iteration " << iters << " iterations\n";
-    std::cout << "MKLDNN: " << calculate_average(mkldnn_durs).count() << " us" << std::endl;
-    std::cout << "MKL: "    << calculate_average(mkl_durs).count()    << " us" << std::endl;  
-
-    for (int i = 0; i < dims_a.height*dims_b.width; i++) {
-      if (mkldnn_ptr_c[i] != mkl_ptr_c[i])
-	std::cout << "mkldnn " << mkldnn_ptr_c[i] << " "
-		  << "mkl " << mkl_ptr_c[i] << std::endl;
-    }
-  }
-};
-
 class batched {
   void mkldnn_matmul(const mat_descriptor& dims_a, const float* a,
 		     const mat_descriptor& dims_b, const float* b,
@@ -253,13 +127,13 @@ class batched {
 
  public:
   void operator()() {
-    constexpr int batch_size = 32;
+    constexpr int batch_size = 128;
     constexpr int head = 8;
     constexpr int m = 256;
     constexpr int k = 256;
     constexpr int n = 64;
     constexpr int warm_up = 10;
-    constexpr int iters = 1000;
+    constexpr int iters = 10000;
   
     auto dims_a = create_matrix_descriptor({batch_size, head, m, k}, false);
     auto dims_b = create_matrix_descriptor({batch_size, head, k, n}, false);
@@ -342,9 +216,6 @@ class batched {
 };
 
 int main() {
-  single s;
-  s();
-  
   batched b;
   b();
   
